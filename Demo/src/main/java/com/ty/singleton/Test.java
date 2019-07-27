7929022efa70113d42f4830a7dc691f48abf7c59
tangyu0001 <423505017@qq.com>
2019-07-28 01:22:52 +0800

    haha

--- Demo/src/main/java/com/ty/singleton/Test.java
+++ Demo/src/main/java/com/ty/singleton/Test.java
@@ -9,24 +9,18 @@ public class Test {
 
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
 
-//        LazySingletonNosafe sin = LazySingletonNosafe.getInstence();
-//        LazySingletonNosafe sin1 = LazySingletonNosafe.getInstence();
-//        LazySingletonNosafe sin2 = LazySingletonNosafe.getInstence();
+        LazySingletonNosafe sin = LazySingletonNosafe.getInstence();
+        LazySingletonNosafe sin1 = LazySingletonNosafe.getInstence();
+        LazySingletonNosafe sin2 = LazySingletonNosafe.getInstence();
 
 
-//        Class clazz = LazySingletonNosafe.class;
-//
-//        Constructor con = clazz.getDeclaredConstructor(null);
-//        con.setAccessible(true);
-//
-//        LazySingletonNosafe sin1 = (LazySingletonNosafe) con.newInstance();
-//        System.out.println(sin );
-//        System.out.println(sin1 );
-//        System.out.println(sin2 );
+        Class clazz = LazySingletonNosafe.class;
 
-            EnumTest e = EnumTest.THREE;
-        System.out.println(e.toString());
-        System.out.println(e.ordinal());
+        Constructor con = clazz.getDeclaredConstructor(null);
+        con.setAccessible(true);
+
+
+
     }
 
 
